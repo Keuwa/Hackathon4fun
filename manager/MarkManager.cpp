aba283ffc7d7fe17e6ba15fb460026b62be32e6e
Meryl Barantal <meryl2194@gmail.com>
2017-04-09 01:37:08 +0200

    add MarkManager

--- manager/MarkManager.cpp
+++ manager/MarkManager.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "MarkManager.h"
+
+
+errorHandler::PersistenceErrorhandler manager::MarkManager::loadAll() {
+    return errorHandler::NOT_FOUND;
+}
+
+errorHandler::PersistenceErrorhandler manager::MarkManager::readOne() {
+    return errorHandler::NOT_FOUND;
+}
+
+errorHandler::PersistenceErrorhandler manager::MarkManager::updateCurrentObject() {
+    return errorHandler::NOT_FOUND;
+}
+
+errorHandler::PersistenceErrorhandler manager::MarkManager::removeCurrentObject() {
+    return errorHandler::NOT_FOUND;
+}
--- manager/MarkManager.h
+++ manager/MarkManager.h
@@ -6,9 +6,29 @@
 #define HACKATHON4FUN_MARKMANAGER_H
 
 
-class MarkManager {
+#include "../model/Mark.h"
+#include "BaseManager.h"
 
-};
+namespace manager {
+    class MarkManager: public BaseManager<MarkManager> {
+
+    private:
+        MarkManager();
+        ~MarkManager();
+    public:
+        std::vector<model::Mark> marks;
+        model::Mark currentMark;
+
+        errorHandler::PersistenceErrorhandler loadAll() override;
+
+        errorHandler::PersistenceErrorhandler readOne() override;
+
+        errorHandler::PersistenceErrorhandler updateCurrentObject() override;
+
+        errorHandler::PersistenceErrorhandler removeCurrentObject() override;
+    };
+}
+
 
 
 #endif //HACKATHON4FUN_MARKMANAGER_H
